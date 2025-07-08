d6e45dc173adfb0ec8577a5256f8e5c2a920d607
eshwargit2 <msselectronic57@gmail.com>
2025-07-08 21:30:41 +0530

    updated

--- frontend/src/admin/Admin.jsx
+++ frontend/src/admin/Admin.jsx
@@ -32,7 +32,6 @@ const handleLogin= async ()=>{
         <h1 className="text-2xl text-center font-bold">Admin Dashboard</h1>
          <section class="mt-4">
           <div class="flex flex-col items-center justify-center w-200">
-      
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
