4265ef6706cdc0435105c27abd16ed08d51c767e
eshwargit2 <msselectronic57@gmail.com>
2025-07-08 21:12:46 +0530

    updated admin page

--- frontend/src/admin/Admin.jsx
+++ frontend/src/admin/Admin.jsx
@@ -30,25 +30,8 @@ const handleLogin= async ()=>{
   return (
     <div>
         <h1 className="text-2xl text-center font-bold">Admin Dashboard</h1>
-       {/* <form onSubmit={(e) => {
-          e.preventDefault();
-          handleLogin();
-       }}>
-        <div className="flex flex-col items-center mt-5">
-          <label htmlFor="adminId" className="mb-2">Admin ID:</label>
-          <input type="text" id="adminId" name="adminId" 
-          onChange={(e) => setUserName(e.target.value)} className="border p-2 rounded mb-4" required />
-
-          <label htmlFor="password" className="mb-2">Password:</label>
-          <input type="password"
-          onChange={(e) => setPassword(e.target.value)} id="password" name="password" className="border p-2 rounded mb-4" required />
-
-          <button type='submit' className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
-          {msg && <p className="text-red-500 mt-2">{msg}</p>}
-        </div>
-       </form> */}
-       <section class="mt-4">
-  <div class="flex flex-col items-center justify-center w-200">
+         <section class="mt-4">
+          <div class="flex flex-col items-center justify-center w-200">
       
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
